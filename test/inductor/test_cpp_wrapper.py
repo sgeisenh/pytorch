6383caec55d0729f8afa891ee8b0b34b0d0a1e29
Bin Bao <binbao@fb.com>
2023-06-02 00:19:03 +0000

    [inductor] Support select_algorithm with cpp_wrapper
    Summary: This is one step towards getting cpp_wrapper work with max_autotune. Switch to use unique kernel name to cache generated cubin file.
    
    ghstack-source-id: 428ec6ca9f2ad9f15edba671b8a90fea2c012dda
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/102738

--- test/inductor/test_cpp_wrapper.py
+++ test/inductor/test_cpp_wrapper.py
@@ -21,6 +21,7 @@ try:
             test_cpu_repro,
             test_foreach,
             test_mkldnn_pattern_matcher,
+            test_select_algorithm,
             test_torchinductor,
             test_torchinductor_dynamic_shapes,
         )
@@ -28,6 +29,7 @@ try:
         import test_cpu_repro
         import test_foreach
         import test_mkldnn_pattern_matcher
+        import test_select_algorithm
         import test_torchinductor
         import test_torchinductor_dynamic_shapes
 except unittest.SkipTest:
@@ -232,6 +234,11 @@ if RUN_CUDA:
             device=None,
             tests=test_foreach.ForeachTests(),
         ),  # test foreach
+        BaseTest(
+            "test_addmm",
+            device=None,
+            tests=test_select_algorithm.TestSelectAlgorithm(),
+        ),
     ]:
         make_test_case(item.name, item.device, item.tests)
 
